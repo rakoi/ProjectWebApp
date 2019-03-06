aaedbb244959a7d859a14568f094230295b65106
bryan rakoi <brianrakoi@gmail.com>
2019-03-06 13:28:15 +0300

    with relationships

--- src/main/java/com/rakoi/webapp/classeye/classeye/ClasseyeApplication.java
+++ src/main/java/com/rakoi/webapp/classeye/classeye/ClasseyeApplication.java
@@ -2,13 +2,21 @@ package com.rakoi.webapp.classeye.classeye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.naming.ConfigurationException;
 
 @SpringBootApplication
-public class ClasseyeApplication {
+public class ClasseyeApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ClasseyeApplication.class, args);
         System.out.println("READY");
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 }
